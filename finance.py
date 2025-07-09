26cc23011b2e31816a43693e3db325e5bb761f68
R.Navya Sasi Priya <priya.22bce20339@vitapstudent.ac.in>
2025-07-09 19:36:22 +0530

    Add files via upload

---
+++ finance.py
@@ -0,0 +1,74 @@
+%PDF-1.3
+%“Œ‹ž ReportLab Generated PDF document http://www.reportlab.com
+1 0 obj
+<<
+/F1 2 0 R /F2 3 0 R
+>>
+endobj
+2 0 obj
+<<
+/BaseFont /Helvetica /Encoding /WinAnsiEncoding /Name /F1 /Subtype /Type1 /Type /Font
+>>
+endobj
+3 0 obj
+<<
+/BaseFont /Helvetica-Bold /Encoding /WinAnsiEncoding /Name /F2 /Subtype /Type1 /Type /Font
+>>
+endobj
+4 0 obj
+<<
+/Contents 8 0 R /MediaBox [ 0 0 612 792 ] /Parent 7 0 R /Resources <<
+/Font 1 0 R /ProcSet [ /PDF /Text /ImageB /ImageC /ImageI ]
+>> /Rotate 0 /Trans <<
+
+>> 
+  /Type /Page
+>>
+endobj
+5 0 obj
+<<
+/PageMode /UseNone /Pages 7 0 R /Type /Catalog
+>>
+endobj
+6 0 obj
+<<
+/Author (anonymous) /CreationDate (D:20250705225129+05'00') /Creator (ReportLab PDF Library - www.reportlab.com) /Keywords () /ModDate (D:20250705225129+05'00') /Producer (ReportLab PDF Library - www.reportlab.com) 
+  /Subject (unspecified) /Title (untitled) /Trapped /False
+>>
+endobj
+7 0 obj
+<<
+/Count 1 /Kids [ 4 0 R ] /Type /Pages
+>>
+endobj
+8 0 obj
+<<
+/Filter [ /ASCII85Decode /FlateDecode ] /Length 356
+>>
+stream
+GasbW9hrS[&4ZCW`@S;CW!&/#pP0b(Xb=Wmncc,Q;BoXT8Li-B@hP7820GWa=\PF%2ku3ALjQ@Ok68YU_rD4-.I[FIQ=dqi@*9olSH:F4qDtI>3r0+S#V;D@:r%1N[*Z@:]sOZ<msolh^[L=WjG+`;@^(jNB.O!(%M3f^(KojrZF)I!+3qq2IP/i9?Zjj_EMoSN\ip%mF&PJQ$="mk7!ZN&:>caSAqZ81OFHm^EnR2AD_#rFF*mC)<.:hlDU0U>Cr"97RQYmLjuq^8_+P#GSrYaUYW<\<Q2ZTlkC6D@g2Z2Z0uud4X,,JN$2-$!`/S0*2I\<ITsPaQ2XV)s=6p5+7V>e\KZOa.[b[k~>endstream
+endobj
+xref
+0 9
+0000000000 65535 f 
+0000000073 00000 n 
+0000000114 00000 n 
+0000000221 00000 n 
+0000000333 00000 n 
+0000000526 00000 n 
+0000000594 00000 n 
+0000000890 00000 n 
+0000000949 00000 n 
+trailer
+<<
+/ID 
+[<f82123be62b1e7e199841202ec91e82e><f82123be62b1e7e199841202ec91e82e>]
+% ReportLab generated PDF document -- digest (http://www.reportlab.com)
+
+/Info 6 0 R
+/Root 5 0 R
+/Size 9
+>>
+startxref
+1395
+%%EOF
